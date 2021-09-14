8efd4f33cbd272ce3adaadf74bb4f69e10a9137b
Jhonathan Weber <jhonmw@gmail.com>
2021-09-14 13:23:12 -0300

    Configurado firebas hosting

---
+++ programacao-imperativa-1/extra/exe001.js
@@ -0,0 +1,8 @@
+//lista de exercícios para estudos de JavaScript passada pelo professor Lucas de PI-1
+
+function areaRetangulo(base, altura) {
+    let perimetro = 2*(base+altura);
+    let area = base * altura;
+    
+    
+}
